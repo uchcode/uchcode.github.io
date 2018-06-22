11f2599602ffb0d8bca506ad759876ab2810bf99
Tom.U <tsutomu.uchima@gmail.com>
2018-06-23 03:51:32 +0900

    Update index.js

--- sandbox/counterapp/index.js
+++ sandbox/counterapp/index.js
@@ -1,6 +1,6 @@
 import './multiple-counter.js'
 
-const multiple = document.createElement('my-multiple-counter')
+const app = document.createElement('my-multiple-counter')
 // multiple.push({defaultCount:10})
 
-document.body.appendChild(multiple)
+document.body.appendChild(app)
