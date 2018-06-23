a5265ef8bba07f2892cf44c2f68cbacfdc3d4e84
Tom.U <tsutomu.uchima@gmail.com>
2018-06-23 17:19:13 +0900

    Update multiple-counter.js

--- sandbox/counterapp/multiple-counter.js
+++ sandbox/counterapp/multiple-counter.js
@@ -1,30 +1,28 @@
 import { Counter, MyCounter } from './counter.js'
 
-class MultipleCounter extends Array {
-  push(options={defaultCount:0}) {
-    return super.push(new Counter(options.defaultCount))
-  }
-  last() {
-    return this[this.length-1]
-  }
-}
-
 const template = `
   <button>add</button>
   <button>rem</button>
-  <button>log</button>
+  <button>info</button>
   <hr>
   <div class="counters"></div>
 `
 
-const makeProxy = that => new Proxy(that.model, {
+class MultipleCounter extends Array {
+  push(options={defaultCount:0}) {
+    return super.push(new Counter(options.defaultCount))
+  }
+}
+
+const MultipleCounterProxy = element => new Proxy(element.model, {
   set(target, property, value, receiver) {
     target[property] = value
+    const counters = element.shadowRoot.querySelector('.counters')
     if (property == 'length') {
-      const c = that.counters.querySelectorAll('my-counter')
+      const c = counters.querySelectorAll('my-counter')
       if (value != c.length) c[c.length-1].remove()
     } else {
-      that.counters.appendChild(new MyCounter(target[property]))
+      counters.appendChild(new MyCounter(target[property]))
     }
     return true
   }
@@ -34,24 +32,16 @@ class MyMultipleCounter extends HTMLElement {
   constructor(multipleCounter) {
     super()
     this.model = multipleCounter || new MultipleCounter()
-    this.proxy = makeProxy(this)
+    this.proxy = MultipleCounterProxy(this)
     this.attachShadow({mode:'open'})
     this.shadowRoot.innerHTML = template
-    const shadow = this.shadowRoot
-    const buttons = shadow.querySelectorAll('button')
-    buttons[0].addEventListener('click', () => this.push())
-    buttons[1].addEventListener('click', () => this.pop())
-    buttons[2].addEventListener('click', () => this.log())
-    this.counters = shadow.querySelector('.counters')
+    const buttons = this.shadowRoot.querySelectorAll('button')
+    buttons[0].addEventListener('click', () => this.proxy.push())
+    buttons[1].addEventListener('click', () => this.proxy.pop())
+    buttons[2].addEventListener('click', () => this.info())
   }
-  push(options={defaultCount:0}) {
-    this.proxy.push(options)
-  }
-  pop() {
-    this.proxy.pop()
-  }
-  log() {
-    console.log(this.proxy)
+  info() {
+    console.info(this.proxy)
   }
 }
 
