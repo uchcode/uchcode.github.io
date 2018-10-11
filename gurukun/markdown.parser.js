7972913942b369339045f1fe035fcb27e3915e7b
Tom.U <tsutomu.uchima@gmail.com>
2018-10-11 16:57:43 +0900

    Update markdown.parser.js

--- gurukun/markdown.parser.js
+++ gurukun/markdown.parser.js
@@ -1,8 +1,4 @@
-document.addEventListener("DOMContentLoaded", async () => {
-
-    var templateUrl = 'https://uchcode.github.io/gurukun/markdown.template.html'
-    var template = await (await fetch(templateUrl)).text()
-
+document.addEventListener('DOMContentLoaded', function () {
 
     var sheet = document.createElement('style')
     var styles = 'body{margin:0;padding:0;}'
@@ -12,14 +8,13 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     var script = document.createElement('script')
     script.src = 'https://cdnjs.cloudflare.com/ajax/libs/showdown/1.8.6/showdown.min.js'
-    script.onload = () => {
-
-        var markdown = document.querySelector('noscript').innerText
+    script.onload = async function () {
 
         var converter = new showdown.Converter({
             emoji: true,
             underline: true,
         })
+        
         converter.setFlavor('github')
 
         converter.addExtension(function () {
@@ -28,46 +23,40 @@ document.addEventListener("DOMContentLoaded", async () => {
                 regex: /<a\shref.+">/g,
                 replace : function (text) {
                     var url = text.match(/"(.*?)"/)[1]
-                    if(url.includes(window.location.hostname) || url[0] == '/' || url[0] == '.' || url[0] == '#'){
+                    if(url.includes(window.location.hostname) || url[0] == '/' || url[0] == '.'){
+                        return '<a href="' + url + '" target="_top">'
+                    }
+                    if(url[0] == '#'){
                         return text
                     }
                     return '<a href="' + url + '" target="_blank">'
                 }
             }]
-        }, 'externalLink')
+        }, 'linkExtension')
 
-        converter.addExtension(function () {
-            return [{
-                type: 'output',
-                regex: /<a\shref.+">/g,
-                replace : function (text) {
-                    var url = text.match(/"(.*?)"/)[1]
-                    if(url.includes(window.location.hostname) || url[0] == '/' || url[0] == '.' || url[0] == '#'){
-                        return '<a href="' + url + '" target="_top">'
-                    }
-                    return text
-                }
-            }]
-        }, 'internalLink')
-
-
+        var templateUrl = 'https://uchcode.github.io/gurukun/markdown.template.html'
+        var template = await (await fetch(templateUrl)).text()
+        var markdown = document.querySelector('noscript').innerText
         var html = converter.makeHtml(markdown)
+        var content = template.replace('${html}', html)
         var iframe = document.createElement('iframe')
+
         document.body.appendChild(iframe)
+
         var doc = iframe.contentWindow.document
         doc.open()
-        doc.write(template.replace('${html}', html))
+        doc.write(content)
         doc.close()
         document.title = document.title || doc.title || doc.body.firstElementChild.firstElementChild.innerText.trim()
-
 
         //handle hash linking
         setTimeout(function() {
             var hash = window.location.hash
             window.location.hash = ''
             window.location.hash = hash
+            iframe.contentWindow.location.hash = ''
+            iframe.contentWindow.location.hash = hash
         }, 100)
-
     }
 
 
